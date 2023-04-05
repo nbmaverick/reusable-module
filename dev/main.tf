68b41c8279c39c0dbd3870e98613d40a3243f1f8
nbmaverick <nbdevops3036@gmail.com>
2023-04-05 22:35:49 +0000

    changed

--- dev/main.tf
+++ dev/main.tf
@@ -1,15 +1,20 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
 module "my_vpc" {
   source = "../module/vpc"
   vpc_cidr = "192.168.0.0/16"
   tenancy = "default"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id = "${module.my_vpc.vpc_id}"
   subnet_cidr = "192.168.1.0/16"
 }
 
 module "my_ec2" {
     source = "../module/ec2"
     ec2_count = 1
+    ami_id = ""    #needs to add ami id from AWS !!!
     instance_type = "t2.micro"
-    subnet_id = "${module.vpc.subnet_id}"
+    subnet_id = "${module.my_vpc.subnet_id}"
 }
 
