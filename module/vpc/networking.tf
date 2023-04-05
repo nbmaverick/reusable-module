aa44be2aa8703506a440fb43c2460de3ec7903cd
nbmaverick <nbdevops3036@gmail.com>
2023-04-05 21:44:00 +0000

    added

--- module/vpc/networking.tf
+++ module/vpc/networking.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "main" {
         Name = "main"
     }
 }
+resource "aws_subnet" "main" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "${var.subnet_cidr}"
+    
+    tags = {
+        Name = "main"
+    }
+}
